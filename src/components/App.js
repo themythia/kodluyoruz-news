2020324a030419e291f78ce0252c0c8565080eca
Emir Al <mythia@windowslive.com>
2022-07-26 21:14:05 +0300

    build: install dependencies tailwindcss, postcss, autoprefixer

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
+import '../index.css';
+
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
+        <p className='text-red-500'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
