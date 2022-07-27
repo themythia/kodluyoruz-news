a61f8ec2cb1531aa74e57fa6d4ed58b4ba9b2a45
Can Gonenli <can.gonenli@hotmail.com>
2022-07-27 19:48:46 +0300

    fix: data fetching

--- src/components/pages/sport/Sport.js
+++ src/components/pages/sport/Sport.js
@@ -1,59 +1,76 @@
 import React, { useEffect, useState } from 'react';
+import { parseFeed } from 'htmlparser2';
+import './Sport.css';
 
 function Sport () {
 
-    const [ text, setText ] = useState(null);
-    // const [ deneme, setDeneme ] = useState(null);
-    // const [ products, setProducts ] = useState([]);
-
-    async function initProducts(){
-        await fetch("https://pacific-caverns-96128.herokuapp.com/https://www.ntv.com.tr/spor.rss")
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => setText(data))
-    }
-
-    // useEffect(() => {
-        
-    //     fetch("https://pacific-caverns-96128.herokuapp.com/https://www.ntv.com.tr/spor.rss")
-    //     .then(response => response.text())
-    //     .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    //     .then(data => setText(data))
-    // }, [])
+    const [ text, setText ] = useState('');
+    const [ txt, setTxt ] = useState({textContent: "", imageContent: ""});
 
     useEffect(() => {
-        initProducts();
-    }, [])
- 
-  return (
-    <>
-    {
-        text !== null ? (
+            fetch("https://pacific-caverns-96128.herokuapp.com/https://www.ntv.com.tr/spor.rss")
+            .then((res) => res.text())
+            .then((data) => setText(data))
+    },[]);
+
+    useEffect(() => {
+        if (text.length > 0){
+            const feed = parseFeed(text);
+            console.log('feed:', feed);
+            let textContents = [{textContent: [], imageContent: []}];
+            if (feed) {
+                for (let i = 0;i<100;i++){
+                    textContents[0].textContent.push(feed.items[i].title)
+                    textContents[0].imageContent.push(feed.items[i].description.split('<img src="')[1].split('?width')[0])
+                }
+                setTxt(textContents);
+            }
+        }    
+    }, [text])
+
+    const order1 = Array.from(Array(10).keys());
+    const order2 = Array.from(Array(10).keys(), x => x + 10);
+    const order3 = Array.from(Array(80).keys(), x => x + 20);
+    // console.log(txt)
+     
+    return (
         <div>
-            <div>
-                <p>{text.getElementsByTagName("entry")[0].childNodes[1].textContent}</p>
-                <img src={text.getElementsByTagName("entry")[0].childNodes[6].textContent.split('<img src="')[1].split('?width')[0]} alt="" width="200px" />
-            </div>
+            {
+                txt[0] ? (
+                    <div>
+                    {
+                        order1.map((item) =>
+                            <div key={item} className="slider">
+                                <img src={txt[0].imageContent[item]} alt=""></img>
+                                <p>{txt[0].textContent[item]}</p>
+                            </div>
+                        )
+                    }
+                    <hr></hr>
+                    {
+                        order2.map((item) =>
+                            <div key={item} className="list">
+                                <img src={txt[0].imageContent[item]} alt=""></img>
+                                <p>{txt[0].textContent[item]}</p>
+                            </div>
+                        )
+                    }
+                    <hr></hr>
+                    {
+                        order3.map((item) =>
+                            <div key={item} className="col">
+                                <img src={txt[0].imageContent[item]} alt=""></img>
+                                <p>{txt[0].textContent[item]}</p>
+                            </div>
+                        )
+                    }
+                    </div>
+                ) 
+                : (<div>Loading...</div>)
+            }
         </div>
-        ) 
-        : (<div>Loading...</div>)
-    }
-    </>
-  )
+    )
 }
 
 export default Sport
 
-
-// return (
-//     <div>
-//         <h1>
-//             {/* deneme */}
-//             {text.getElementsByTagName("entry")[0].childNodes[1].textContent}
-//         </h1>
-//     </div>
-//   )
-
-// if (typeof box === 'object' && box !== null && 'getElementsByTagName' in box) { 
-//     const result = box.getElementsByTagName('p'); 
-//   }
