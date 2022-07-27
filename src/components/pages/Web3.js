b565e2147a80e2581ba0aa790f93ba4c908ab05e
Argedik <gedikas@hotmail.com>
2022-07-27 13:39:25 +0300

    chore: Change all files in src

---
+++ src/components/pages/Web3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../shared/Footer';
+import Header from '../shared/Header';
+
+const Web3 = () => {
+  return (
+    <>
+      test
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Web3;
---
+++ src/components/shared/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <div className='nav_bar w-full h-full'>
+        <header className='nav_bar w-16 h-16'>test</header>
+        <header className='nav_bar w-16 h-16'>test</header>
+        <header className='nav_bar w-16 h-16'>test</header>
+      </div>
+    </>
+  );
+};
+
+export default Header;
