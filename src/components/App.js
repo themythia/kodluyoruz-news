556b897517a336276c94ad9a0a35c8257aa06ceb
HazalCevikk <hazal.cevikk@gmail.com>
2022-07-31 02:11:21 +0300

    style: added responsive design properties

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
 import '../index.css';
+import Sport from './pages/Sport/Sport';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p className='text-red-500'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Sport/>
+   </>
   );
 }
 
