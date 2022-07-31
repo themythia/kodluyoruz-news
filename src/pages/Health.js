0c4cbcfc7c612dec68c3a482734d3dceb09cfb07
Kadir Özsoy <104651899+kozsoy@users.noreply.github.com>
2022-07-31 23:16:06 +0300

    Add files via upload

---
+++ src/pages/Health.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import "./Health.css"
+
+
+
+const Health = () => {
+  // const newArray=Array(15).fill('mahmut');
+  // console.log(newArray)
+
+  
+
+
+  return (
+     <>
+        <div className='header'>NTV</div>
+        <div className='navbar'>Sağlık</div>
+        <div className='altNavbar'>Sağlık Haberleri</div>
+        <div className='slickSlider'>Slider Alanı</div>
+        <div className='slider2'>2. slider</div>
+        <div className='listItem'>liste1</div>
+        <div className='listItem2'>liste2</div>
+        {/* <div className='listItem2'>{newArray.map(function(each,key){return <div key={key} > {each}</div>})}</div> */}
+        <div className='multipleItems'>hata düzelticek </div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='multipleItems'>multiple items</div>
+        <div className='showMore'>Daha Fazla Göster</div>
+        <div className='footerCopyRight'>NTV icon copyright</div>
+        <div className='footer'>Kurumsal</div>
+        <div className='footerIcons '>Ntv'yi Keşfet</div>
+
+
+        
+      
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Health
---
+++ src/pages/Life.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import "./Life.css"
+
+const Health = () => {
+  return (
+     <container>
+        <div> 
+              <div className='header'>Life</div>
+              <div className='navbar'>Kategori</div>
+        </div>
+        <div className='slickSlider'>Slider Alanı</div>
+        <div className='listItem'>
+            <div className='listItem2'>list1</div>
+            <div className='listItem2'>list2</div>
+            <div className='listItem2'>list3</div>
+        </div>
+        {/* 2li card */}
+        <div className='card'> 
+            <div className='card1'>card1</div>
+            <div className='card2'>card2</div>
+        </div>
+        <div className='content'>
+              <div className='contentAll'>
+                1
+              </div>
+              <div className='contentAll'>
+                2
+              </div>
+              <div className='contentAll'>
+                3
+              </div>
+        </div>
+        <div className='redCard'>
+          <div className='redCardMain'>
+              <div className='redCardIn'></div>
+              <div className='redCardIn'></div>
+              <div className='redCardIn'></div>
+              <div className='redCardIn'></div>
+              <div className='redCardIn'></div>
+              <div className='redCardIn'></div>
+          </div>
+        </div>
+        <div className='ntvContent'>
+          <ul className='ulCard'>
+            <li className='listCard'>
+              1
+            </li>
+            <li className='listCard'>
+              2
+            </li>
+            <li className='listCard'>
+              3
+            </li>
+          </ul>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          {/* <div className='listItem2'>liste2</div> */}
+          {/* <div className='multipleItems'>hata düzelticek </div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='multipleItems'>multiple items</div>
+          <div className='showMore'>Daha Fazla Göster</div> */}
+          {/* <div className='footerCopyRight'>NTV copyright</div> */}
+          {/* <div className='footer'>Kurumsal</div> */}
+          <div className='footerIcons '>Ntv'yi Keşfet</div>
+
+
+        
+      
+    
+    
+    
+    
+    
+    
+    </container>
+  )
+}
+
+export default Health
