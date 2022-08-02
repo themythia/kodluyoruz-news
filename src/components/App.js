9af16e1caa5a8f900f7c1cb5f6db0a3df41d41a4
Yusuf Avcilar <yusufavcilar53@gmail.com>
2022-08-02 22:24:28 +0300

    Refactor files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import '../index.css';
 import { Routes, Route } from 'react-router-dom';
-import Art from '../components/pages/Art';
+import Art from '../components/pages/artpage/Art';
 import NotFound from './pages/NotFound';
 import HomePage from './pages/homepage/HomePage';
 
--- src/components/pages/artpage/Art.js
+++ src/components/pages/artpage/Art.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import ArtCard from '../ArtCard';
+import ArtCard from './ArtCard';
 import { parseFeed } from 'htmlparser2';
 
 const Art = () => {
