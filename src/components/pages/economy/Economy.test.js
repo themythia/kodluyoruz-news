defd344571ddb4a5ebb3b66d2f28907f654b349b
Emir Al <mythia@windowslive.com>
2022-08-05 11:34:26 +0300

    test: fix economy page tests

--- src/components/pages/economy/Economy.test.js
+++ src/components/pages/economy/Economy.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Economy from './Economy';
 
 // useNavigate hook mock
@@ -11,21 +12,21 @@ jest.mock('react-router-dom', () => ({
 }));
 
 test('economy: renders title bar', () => {
-  render(<Economy />);
+  render(<Economy />, { wrapper: BrowserRouter });
   const title = screen.getByTestId('title');
   expect(title).toBeInTheDocument();
 });
 
 test('economy: renders mobile news cards', () => {
-  // app should render 80 news cards
-  render(<Economy />);
+  // app should render 30 news cards
+  render(<Economy />, { wrapper: BrowserRouter });
   const newsCards = screen.getAllByTestId('newsCard');
-  expect(newsCards).toHaveLength(80);
+  expect(newsCards).toHaveLength(30);
 });
 
 test('economy: renders currency lists', () => {
   // mobile layout should render 2 currency lists
-  render(<Economy />);
+  render(<Economy />, { wrapper: BrowserRouter });
   const currencyLists = screen.getAllByTestId('currency-list');
   expect(currencyLists).toHaveLength(2);
 });
@@ -34,14 +35,14 @@ test('economy: renders carousel items', () => {
   // we want 20 slides in the carousel
   // but react-slick renders 2n+1 slides,
   // which means app should render 41 slides
-  render(<Economy />);
+  render(<Economy />, { wrapper: BrowserRouter });
   const carouselItems = screen.getAllByTestId('carousel-item');
   expect(carouselItems).toHaveLength(41);
 });
 
 test('economy: renders carousel buttons', () => {
   // app should render 20 carousel buttons
-  render(<Economy />);
+  render(<Economy />, { wrapper: BrowserRouter });
   const carouselButtons = screen.getAllByTestId('carousel-button');
   expect(carouselButtons).toHaveLength(20);
 });
