8786f0ba80b32e9573108337c6cc99dd994c0645
HazalCevikk <hazal.cevikk@gmail.com>
2022-08-08 21:59:09 +0300

    style: add some tailwind classes

---
+++ src/components/pages/Astroloji/Astrology.test.js
@@ -0,0 +1,19 @@
+// import {screen, render} from "@testing-library/react";
+// import {userEvent} from "@testing-library/user-event";
+// import Astrology from "./Astrology";
+
+// describe("Astrology testing", ()=>{
+//     let link;
+
+//     beforeEach(()=>{
+//         //her testten önce çalışır
+//         render(<Astrology/>);
+//         link = screen.getByText("https://ntv.com.tr")
+
+//     });
+
+//     test('should go to the link', () => { 
+//         userEvent.click(link);
+//         expect(screen.getByRole('link')).toHaveAttribute('href', 'https://ntv.com.tr')
+//     })
+// })
