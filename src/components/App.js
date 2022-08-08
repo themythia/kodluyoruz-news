3719fb32613abcd3cff101cf494d22585d5c088d
Can Gonenli <102178923+cngnnli@users.noreply.github.com>
2022-08-08 10:46:52 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import '../index.css';
 import React from 'react';
-// import Sport from './pages/sport/Sport';
 import Technology from './pages/technology/Technology';
 
 function App() {
   return (
     <div className='App'>
-      {/* <Sport /> */}
       <Technology />
     </div>
   );
