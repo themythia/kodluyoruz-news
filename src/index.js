0641a75a8f7305eaa2afc13414b95740af2865bb
Argedik <gedikas@hotmail.com>
2022-08-09 11:31:41 +0300

    add

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
