0183bf95c92d5c2cbf1549736fd1aaf8f7df254e
Argedik <gedikas@hotmail.com>
2022-08-10 18:41:07 +0300

    Some route changes

--- src/components/App.js
+++ src/components/App.js
@@ -17,49 +17,55 @@ import Tourism from './pages/tourism/Tourism';
 import Turkey from './pages/turkey/Turkey';
 import Art from '../components/pages/artpage/Art';
 import HomePage from './pages/homepage/HomePage';
+import Header from './shared/Header/Header';
+import Footer from './shared/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='*' element={<NotFound />} />
-      <Route path='/economy'>
-        <Route index={true} element={<Economy />} />
-        <Route path='details' element={<EconomyDetails />} />
-      </Route>
-      <Route path='/life'>
-        <Route index={true} element={<Life />} />
-        <Route path='details' element={<LifeDetails />} />
-      </Route>
-      <Route path='/astrology'>
-        <Route index={true} element={<Astrology />} />
-        <Route path='details' element={<AstrologyDetails />} />
-      </Route>
-      <Route path='/sport'>
-        <Route index={true} element={<Sport />} />
-        <Route path='details' element={<SportDetails />} />
-      </Route>
-      {/* Otomobil sayfası */}
-      <Route path='/art'>
-        <Route index={true} element={<Art />} />
-      </Route>
-      <Route path='/turkey'>
-        <Route index={true} element={<Turkey />} />
-        <Route path='details' element={<HealthDetails />} />
-      </Route>
-      <Route path='/health'>
-        <Route index={true} element={<Health />} />
-        <Route path='details' element={<HealthDetails />} />
-      </Route>
-      <Route path='/turizm'>
-        <Route index={true} element={<Tourism />} />
-        <Route path='details' element={<HealthDetails />} />
-      </Route>
-      <Route path='/technology'>
-        <Route index={true} element={<Technology />} />
-        <Route path='details' element={<TechnologyDetails />} />
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path='/economy'>
+          <Route index={true} element={<Economy />} />
+          <Route path='details' element={<EconomyDetails />} />
+        </Route>
+        <Route path='/life'>
+          <Route index={true} element={<Life />} />
+          <Route path='details' element={<LifeDetails />} />
+        </Route>
+        <Route path='/astrology'>
+          <Route index={true} element={<Astrology />} />
+          <Route path='details' element={<AstrologyDetails />} />
+        </Route>
+        <Route path='/sport'>
+          <Route index={true} element={<Sport />} />
+          <Route path='details' element={<SportDetails />} />
+        </Route>
+        {/* Otomobil sayfası */}
+        <Route path='/art'>
+          <Route index={true} element={<Art />} />
+        </Route>
+        <Route path='/turkey'>
+          <Route index={true} element={<Turkey />} />
+          <Route path='details' element={<HealthDetails />} />
+        </Route>
+        <Route path='/health'>
+          <Route index={true} element={<Health />} />
+          <Route path='details' element={<HealthDetails />} />
+        </Route>
+        <Route path='/turizm'>
+          <Route index={true} element={<Tourism />} />
+          <Route path='details' element={<HealthDetails />} />
+        </Route>
+        <Route path='/technology'>
+          <Route index={true} element={<Technology />} />
+          <Route path='details' element={<TechnologyDetails />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
